f16c75bdb07e5b8bafdb5ec62e37e07119d1fea6
scott.mather <scott.mather@mindcandy.com>
2014-02-19 10:25:12 +0000

    Initial check in of Root Race tutorial

---
+++ Robot Race Finished UnityProject/Robot Race/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour 
+{
+
+	public Transform enemyRobot;
+	//Reference to a game object that is the finish line
+	public Transform finishLine;
+
+	//controls how fast the enemy moves
+	public float movementSpeed;
+
+	void Update () 
+	{
+		//Move the enemy in the x direction by "movementSpeed" each frame
+		enemyRobot.Translate(movementSpeed,0,0);
+
+		//Checks the position of the enemy against the position of the finish line
+		if(enemyRobot.position.x > finishLine.position.x)
+		{
+			Application.LoadLevel("LoseScene");
+		}
+	}
+}
---
+++ Robot Race Finished UnityProject/Robot Race/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinCondition : MonoBehaviour 
+{
+	//Stores a reference to the players robot
+	public Transform playerRobot;
+	//Reference to a game object that is the finish line
+	public Transform finishLine;
+
+	void Update () 
+	{
+		//if the players x position is greater than the finish lines x position, change the scene 
+		if(playerRobot.position.x > finishLine.position.x)
+		{
+			Application.LoadLevel("WinScene");
+		}	
+	}
+}
