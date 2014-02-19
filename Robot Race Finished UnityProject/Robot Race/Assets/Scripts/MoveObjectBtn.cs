bce3c9ae7ba713fb4e04ec7bcabca815b48aa34c
scott.mather <scott.mather@mindcandy.com>
2014-02-19 10:25:12 +0000

    Initial check in of Robot Race tutorial

---
+++ Robot Race Finished UnityProject/Robot Race/Assets/Scripts/MoveObjectBtn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObjectBtn : MonoBehaviour 
+{
+	//Stores a reference to the players robot so we can move it whenever the button is clicked
+	public Transform playerRobot;
+
+	//The speed that the robot will move across the stage
+	public float movementSpeed = 0.2f;
+
+	void OnMouseDown()
+	{
+		//Move the player in the x direction by "movementSpeed" each frame
+		playerRobot.Translate(movementSpeed,0,0);
+	}
+}
